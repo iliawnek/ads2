df979fa7da13dee033ccd6f5887b63660b80d6b3
Ken Li <iliawnek@gmail.com>
2016-02-03 17:40:05 +0000

    Reformatted ArrayQueue.java code

--- ex02/ArrayQueue.java
+++ ex02/ArrayQueue.java
@@ -3,53 +3,65 @@ import java.io.*;
 
 public class ArrayQueue<E> {
 
-    public static final int CAPACITY = 10;  // default queue capacity
-    private E[] Q;                          // E array used to implement the queue  
-    private int n;                          // actual capacity of queue
-    private int front;                      // index for the top of queue
-    private int rear;                       // rear of the queue
-    private int size;                       // size of the queue
-    
-	
-    public ArrayQueue(){this(CAPACITY);}
+    public static final int DEFAULT_CAPACITY = 10;  // default queue capacity
+    private E[] queue;      // E array used to implement the queue
+    private int capacity;   // actual capacity of queue
+    private int front;      // index for the top of queue
+    private int rear;       // rear of the queue
+    private int size;       // size of the queue
+
+    public ArrayQueue() {
+        this(DEFAULT_CAPACITY);
+    }
 
     @SuppressWarnings("unchecked")
-    public ArrayQueue(int capacity){
-	n = capacity;
-	Q = (E[]) new Object [n]; 
-	front = rear = size = 0;
+    public ArrayQueue(int capacity) {
+        this.capacity = capacity;
+        queue = (E[]) new Object[this.capacity];
+        front = rear = size = 0;
     }
     //
-    // NOTE: java does not allow creation of array with parametrised type!
+    // NOTE: java does not allow creation of array with parameterised type!
     //
-	
-    public int size(){return -999;}
-    //
-    // IMPLEMENT ME
-    //
-	 
-    public boolean isEmpty(){return false;}
+
+    public int size() {
+        return -999;
+    }
     //
     // IMPLEMENT ME
     //
 
-    public E front() throws ArrayQueueException {return null;}
-    //
-    // IMPLEMENT ME
-    //
-	
-    public void enqueue(E element) throws ArrayQueueException {}
+    public boolean isEmpty() {
+        return false;
+    }
     //
     // IMPLEMENT ME
     //
 
-    
-    public E dequeue() throws ArrayQueueException {return null;}
+    public E front() throws ArrayQueueException {
+        return null;
+    }
     //
     // IMPLEMENT ME
     //
-    
-    public String toString(){return "";}
+
+    public void enqueue(E element) throws ArrayQueueException {
+    }
+    //
+    // IMPLEMENT ME
+    //
+
+
+    public E dequeue() throws ArrayQueueException {
+        return null;
+    }
+    //
+    // IMPLEMENT ME
+    //
+
+    public String toString() {
+        return "";
+    }
     //
     // IMPLEMENT ME
     //
@@ -59,4 +71,4 @@ public class ArrayQueue<E> {
     //       if the queue is empty return "[]"
     //
 }
-	
+
