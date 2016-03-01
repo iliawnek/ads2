7724b08af05960644f86ee1ff495aa4cd07b3efc
iliawnek <iliawnek@gmail.com>
2016-03-01 15:23:57 +0000

    Added javadoc comments for all methods

--- ex04/Heap.java
+++ ex04/Heap.java
@@ -4,6 +4,7 @@ public class Heap<E extends Comparable<E>> {
     private int last;     // index of last element in heap
     private int capacity; // max number of elements in heap
 
+
     /**
      * create a heap with capacity n
      */
@@ -13,6 +14,7 @@ public class Heap<E extends Comparable<E>> {
         last = 0;
     }
 
+
     /**
      * by default, create a new heap with capacity = 7 (height = 3)
      */
@@ -20,10 +22,12 @@ public class Heap<E extends Comparable<E>> {
         this(7);
     }
 
+
     @SuppressWarnings("unchecked")
     private int compare(Object x, Object y) {
         return ((E) x).compareTo((E) y);
     }
+
 
     /**
      * @return number of elements in the heap
@@ -32,12 +36,14 @@ public class Heap<E extends Comparable<E>> {
         return last;
     }
 
+
     /**
      * @return true if heap is empty
      */
     public boolean isEmpty() {
         return last == 0;
     }
+
 
     /**
      * @return element with smallest key (without removing it)
@@ -55,32 +61,30 @@ public class Heap<E extends Comparable<E>> {
     }
 
 
+    /**
+     * @param e is the element to be inserted into the heap
+     * @throws HeapException if the heap is full
+     */
     public void insert(E e) throws HeapException {
     }
-    //
-    // inserts e into the heap
-    // throws exception if heap overflow
-    //
 
+
+    /**
+     * @return the minimum element, and remove it from the heap
+     * @throws HeapException if the heap is empty
+     */
     @SuppressWarnings("unchecked")
     public E removeMin() throws HeapException {
         return null;
     }
-    //
-    // removes and returns smallest element of the heap
-    // throws exception if heap is empty
-    // NOTE: must cast result to class (E)
-    //       see min() above
-    //
 
+
+    /**
+     * @return elements in H in the order H[1] to H[last]
+     */
     public String toString() {
         return "implement me";
     }
-    //
-    // outputs the entries in H in the order H[1] to H[last]
-    // in same style as used in ArrayQueue
-    // 
-    // 
 
 }
 
