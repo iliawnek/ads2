a0d614b2acf36d5ae3cc4596dc75afbe88caa8ab
iliawnek <iliawnek@gmail.com>
2016-03-01 15:53:02 +0000

    Implemented minimum finding method

--- ex04/Heap.java
+++ ex04/Heap.java
@@ -16,16 +16,39 @@ public class Heap<E extends Comparable<E>> {
 
 
     /**
-     * by default, create a new heap with capacity = 7 (height = 3)
+     * by default, create a new heap with capacity = 7 (max height = 3)
      */
     public Heap() {
         this(7);
     }
 
 
+    /**
+     * @param x is an object x you want to compare with object y
+     * @param y is an object y you want to compare with object x
+     * @return < 0 if x < y
+     *          = 0 if x == y
+     *          > 0 if x > y
+     */
     @SuppressWarnings("unchecked")
     private int compare(Object x, Object y) {
         return ((E) x).compareTo((E) y);
+    }
+
+
+    /**
+     * @param x is index of element x in heap you want to swap with element y
+     * @param y is index of element y in heap you want to swap with element x
+     * @throws HeapException if x and y do not refer to elements in the heap.
+     */
+    @SuppressWarnings("unchecked")
+    private void swap(int x, int y) throws HeapException {
+        if (x > last || y > last || x < 0 || y < 0) {
+            throw new HeapException("Indices x and y must refer to an element within the heap.");
+        }
+        E temp = (E) H[x];
+        H[x] = H[y];
+        H[y] = temp;
     }
 
 
@@ -46,18 +69,48 @@ public class Heap<E extends Comparable<E>> {
 
 
     /**
+     * @return true if heap is full
+     */
+    public boolean isFull() {
+        return last == capacity;
+    }
+
+
+    /**
      * @return element with smallest key (without removing it)
      * @throws HeapException if heap is empty
      */
     @SuppressWarnings("unchecked")
     public E min() throws HeapException {
+        return (E) H[minIndex()];
+    }
+
+
+    /**
+     * @return the index of the minimum element in the heap
+     */
+    @SuppressWarnings("unchecked")
+    private int minIndex() throws HeapException {
         if (isEmpty()) {
-            throw new HeapException("Heap underflow: cannot return an element from an empty heap.");
+            throw new HeapException("Heap underflow: cannot get element from an empty heap.");
         }
 
+        // if only one element in heap, return it (minimum by default)
+        if (size() == 1) {
+            return 1;
+        }
 
+        int minIndex = 1;
+        E minElement = (E) H[minIndex];
+        for (int i = 2; i <= size(); i++) {
+            // store new minElement and minIndex if less than current min
+            if (compare(minElement, H[i]) < 0) {
+                minIndex = i;
+                minElement = (E) H[minIndex];
+            }
+        }
 
-        return (E) H[1];
+        return minIndex;
     }
 
 
@@ -66,6 +119,9 @@ public class Heap<E extends Comparable<E>> {
      * @throws HeapException if the heap is full
      */
     public void insert(E e) throws HeapException {
+        if (isFull()) {
+            throw new HeapException("Heap overflow: cannot insert an element into a full heap.");
+        }
     }
 
 
@@ -75,6 +131,9 @@ public class Heap<E extends Comparable<E>> {
      */
     @SuppressWarnings("unchecked")
     public E removeMin() throws HeapException {
+        if (isEmpty()) {
+            throw new HeapException("Heap underflow: cannot remove an element from an empty heap.");
+        }
         return null;
     }
 
